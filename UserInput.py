4e2f6c9e2c37e1e64f71b900a1bb53416fe2e9d9
kimste2 <steven_kim@student.uml.edu>
2017-06-06 15:19:11 -0400

    completed addition function

--- UserInput.py
+++ UserInput.py
@@ -1,3 +1,10 @@
 # takes 2 numbers and preforms basic operations on them
-int_one = input("Please enter a number: ")
-int_two = input("Please enter another number: ")
+def addition(num1, num2):
+    return num1 + num2
+
+int_one = float(input("Please enter a number: "))
+int_two = float(input("Please enter another number: "))
+
+sum = addition(int_one, int_two)
+
+print("The sum of %.2lf and %.2lf is %.2lf " % (int_one, int_two, sum))
