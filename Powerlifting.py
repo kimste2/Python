892061eb54d2200715874d9d0fe9470ae2810c8f
kimste2 <steven_kim@student.uml.edu>
2017-06-20 17:32:52 -0400

    Revising user input

--- Powerlifting.py
+++ Powerlifting.py
@@ -3,7 +3,7 @@ Powerlifitng is a sport composed of three lifts: the squat, bench and deadlift i
 There are three attempts at each lift. The winner is whom ever has the highest total. Competitions
 are held in either kilos or pounds. 
 '''
-
+lift_attempts = 3
 lifter = {
     "name":[],
     "units":[],
@@ -18,3 +18,14 @@ print(lifter["name"])
 lifter["units"] = input("Please enter competition units: \"kilos\" or \"pounds\": ")
 print(lifter["units"])
 
+def get_squat():
+    for x in range(0,3):
+        make_lift = input("Did you make your squat attempt? ")
+        make_lift.lower()
+        if make_lift != "yes" and make_lift != "no":
+            print("Invalid input! \n")
+            get_squat()
+        else:
+            attempt = input("Please enter weight lifting: ")
+            
+get_squat()
