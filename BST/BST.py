23b10f849b3f82dd5ba8dee740567cf5e3c45ff4
kimste2 <steven_kim@student.uml.edu>
2017-07-24 14:52:51 -0400

    Node class

--- BST/BST.py
+++ BST/BST.py
@@ -1,4 +1,15 @@
 # BST
-###
-use a BST to store numbers 
-### 
+
+#use a BST to store numbers from a text file 
+
+class Node:
+   
+    def __init__(self,key):
+        self.rChild = None
+        self.lChild = None
+        self.data = key
+	
+
+
+
+
